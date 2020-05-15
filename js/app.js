cecac6e6e2811a8165e55395c4215022d5a10266
Peterturn <55192090+Peterturn@users.noreply.github.com>
2020-05-15 19:12:07 +0200

    working on fetch functions

--- js/app.js
+++ js/app.js
@@ -1,26 +1,28 @@
 /** 
  * Var Selectors 
-*/
+ */
 const gallery = document.getElementById('gallery');
 const testZone = document.getElementById('test');
-let moduleHTML = [];
+let employeeArr = [];
 
 /** 
  * Fetch protocols 
-*/
+ */
 fetch('https://randomuser.me/api/?results=12')
-  .then(response => response.json())
-  .then((data) => {generateEmployees(data.results);
+    .then(response => response.json())
+    .then((data) => {
+        employeeArr.push(data.results);
+        generateEmployees(data.results)
     })
-  
+
 /** 
  * functions
-*/
+ */
 function generateEmployees(data) {
-    let html;
     /** 
-    * employees small cards
-    */
+     * employees small cards
+     */
+    let html;
     data.map(item =>
         html += ` 
             <div class="card" id="${item.name.first}">
@@ -30,48 +32,52 @@ function generateEmployees(data) {
                 <div class="card-info-container">
                     <h3 id="name" class="card-name cap">${item.name.first} ${item.name.last}</h3>
                     <p class="card-text">${item.email}</p>
-                    <p class="card-text cap">${item.location.city}, ${item.location.state}</p>
+                    <p class="card-text cap">${item.location.city}</p>
                 </div>    
             </div>
                 `);
 
-    /** 
-    * employees module cards
-    */
-    data.map(modItem =>
-        //let birthday = item.dob.date.slice(0,9); 
-        moduleHTML += `{${modItem.name.first} : 
-            
-            <div class="modal-container" id="${modItem.name.first}">
-                        <div class="modal">
-                            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                            <div class="modal-info-container">
-                                <img class="modal-img" src="${modItem.picture.large}" alt="profile picture ${modItem.name.last}">
-                                <h3 id="name" class="modal-name cap">${modItem.name.first} ${modItem.name.last}</h3>
-                                <p class="modal-text">${modItem.email}</p>
-                                <p class="modal-text cap">${modItem.location.city}</p>
-                                <hr>
-                                <p class="modal-text">${modItem.phone}</p>
-                                <p class="modal-text">${modItem.location.street.number} ${modItem.location.street.name}, ${modItem.location.city}, ${modItem.location.state} ${modItem.location.postcode}</p>
-                                <p class="modal-text">Birthday: ${modItem.dob.date.slice(0,9)}</p>
-                            </div>
-                        </div>
-                        }`,);
-        
-
-    gallery.innerHTML = html;  
+    gallery.innerHTML = html;
 }
 
 
+function bigCard(employeeInfo) {
+    /** 
+     * employees module cards
+     */
+    let moduleHTML;
+    employeeData.map(modItem =>
+        //let birthday = item.dob.date.slice(0,9); 
+        moduleHTML += `{${modItem.name.first} : 
+           
+           <div class="modal-container" id="${modItem.name.first}">
+                       <div class="modal">
+                           <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+                           <div class="modal-info-container">
+                               <img class="modal-img" src="${modItem.picture.large}" alt="profile picture ${modItem.name.last}">
+                               <h3 id="name" class="modal-name cap">${modItem.name.first} ${modItem.name.last}</h3>
+                               <p class="modal-text">${modItem.email}</p>
+                               <p class="modal-text cap">${modItem.location.city}</p>
+                               <hr>
+                               <p class="modal-text">${modItem.phone}</p>
+                               <p class="modal-text">${modItem.location.street.number} ${modItem.location.street.name}, ${modItem.location.city}, ${modItem.location.state} ${modItem.location.postcode}</p>
+                               <p class="modal-text">Birthday: ${modItem.dob.date.slice(0,9)}</p>
+                           </div>
+                       </div>
+                       }`, );
+
+    testZone.innerHTML = moduleHTML
+}
+
 /** 
  * 
-*/
+ */
 let cards = document.getElementsByClassName('card');
 gallery.addEventListener('click', (e) => {
     let eTarget = e.target;
-    for (let i=0; i<cards.length; i++){
-        if(eTarget === cards[i]){
-        console.log("clicked");
+    for (let i = 0; i < cards.length; i++) {
+        if (eTarget === cards[i]) {
+            console.log("clicked");
         }
     }
 });
