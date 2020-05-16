50493e1fdb63b0e7890d2d7bd34e31ad41b4c002
Peterturn <55192090+Peterturn@users.noreply.github.com>
2020-05-16 20:16:28 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -12,7 +12,8 @@ fetch('https://randomuser.me/api/?results=12')
     .then(response => response.json())
     .then((data) => {
         employeeArr.push(data.results);
-        generateEmployees(data.results)
+        generateEmployees(data.results);
+        eListener(data);
     })
 
 /** 
@@ -50,49 +51,40 @@ function bigCard(data) {
 
 
 
-    let moduleHTML = '';
-    employeeData.map(modItem =>
-        //let birthday = item.dob.date.slice(0,9); 
-        moduleHTML += `{${modItem.name.first} : 
+//     let moduleHTML = '';
+//     employeeData.map(modItem =>
+//         //let birthday = item.dob.date.slice(0,9); 
+//         moduleHTML += `{${modItem.name.first} : 
            
-           <div class="modal-container" id="${modItem.name.first}">
-                       <div class="modal">
-                           <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
-                           <div class="modal-info-container">
-                               <img class="modal-img" src="${modItem.picture.large}" alt="profile picture ${modItem.name.last}">
-                               <h3 id="name" class="modal-name cap">${modItem.name.first} ${modItem.name.last}</h3>
-                               <p class="modal-text">${modItem.email}</p>
-                               <p class="modal-text cap">${modItem.location.city}</p>
-                               <hr>
-                               <p class="modal-text">${modItem.phone}</p>
-                               <p class="modal-text">${modItem.location.street.number} ${modItem.location.street.name}, ${modItem.location.city}, ${modItem.location.state} ${modItem.location.postcode}</p>
-                               <p class="modal-text">Birthday: ${modItem.dob.date.slice(0,9)}</p>
-                           </div>
-                       </div>
-                       }`, );
+//            <div class="modal-container" id="${modItem.name.first}">
+//                        <div class="modal">
+//                            <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+//                            <div class="modal-info-container">
+//                                <img class="modal-img" src="${modItem.picture.large}" alt="profile picture ${modItem.name.last}">
+//                                <h3 id="name" class="modal-name cap">${modItem.name.first} ${modItem.name.last}</h3>
+//                                <p cla="modal-text">${modItem.email}</p>
+//                                <p class="modal-text cap">${modItem.location.city}</p>
+//                                <hr>
+//                                <p class="modal-text">${modItem.phone}</p>
+//                                <p class="modal-text">${modItem.location.street.number} ${modItem.location.street.name}, ${modItem.location.city}, ${modItem.location.state} ${modItem.location.postcode}</p>
+//                                <p class="modal-text">Birthday: ${modItem.dob.date.slice(0,9)}</p>
+//                            </div>
+//                        </div>
+//                        }`, );
 
-    testZone.innerHTML = moduleHTML
-}
+//     testZone.innerHTML = moduleHTML
+// }
 
 /** 
  * 
  */
 
- //----- TEST CODE -----//
- let cards = document.getElementsByClassName('card');
- cards.forEach(item => {
-     item.addEventListener('click', event => {
-         let eTarget = event.target;
-         console.log(eTarget);
-
-     });
- });
-// let cards = document.getElementsByClassName('card');
-// gallery.addEventListener('click', (e) => {
-//     let eTarget = e.target;
-//     for (let i = 0; i < cards.length; i++) {
-//         if (eTarget === cards[i]|| or) {
-//             console.log("clicked");
-//         }
-//     }
-// });
+//----- TEST CODE -----//
+function eListener(data) {
+    let cards = document.getElementsByClassName('card');
+    for (let i = 0; i < cards.length; i++) {
+        cards[i].addEventListener('click', event => {
+            console.log(cards[i]);
+        });
+    }
+}
